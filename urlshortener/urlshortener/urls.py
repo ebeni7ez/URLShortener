58170dfc9104c539cde58455d161655aaa7ee576
ebeni7ez <egbm91@gmail.com>
2015-05-04 14:38:32 +0200

    fixed some stuff

--- urlshortener/urlshortener/urls.py
+++ urlshortener/urlshortener/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('urls.urls')),
+    url(r'^$', 'urls.views.index', name='index'),
+    url(r'^(?P<short_url>\w+)$', 'urls.views.redirect_short_url', name='redirect_short_url'),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                            {'document_root': settings.STATIC_ROOT}),
 )
